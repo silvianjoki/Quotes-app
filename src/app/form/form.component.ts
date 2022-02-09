a14f66d3edb2ed852483432381066895e0f96f10
Silvia Njoki <10silvianjoki@gmail.com>
2022-02-09 14:45:16 +0300

    form changes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Quotes} from '../quotes'
@@ -10,16 +10,32 @@ import { Quotes} from '../quotes'
 })
 export class FormComponent implements OnInit {
 
-  newQuote = new Quotes ('','','',new Date(),0,0);
-@Output() addQuote = new EventEmitter < Quotes > ();
+  quoted= new Quotes ('Silvia','Take some rest', 'Silvie');
+  @Output() add = new EventEmitter < Quotes > ();
   name !:string;
   title !:string;
   quote !:string;
   author !: string;
 
-  submitQuote(){
-    this.addQuote.emit(this.newQuote)
+  addQuote(){
+    this.name ='';
+    this.title = '';
+    this.quote = '';
+    this.author = '';
+    this.add.emit(this.quoted);
+    // this.quoted = new Quote('','','','', new Date(),0,0);
   }
+
+//   newQuote = new Quotes ('','','',);
+// @Output() addQuote = new EventEmitter < Quotes > ();
+//   name !:string;
+//   title !:string;
+//   quote !:string;
+//   author !: string;
+
+//   submitQuote(){
+//     this.add.emit(this.newQuote)
+//   // }
 
   constructor() { }
 
