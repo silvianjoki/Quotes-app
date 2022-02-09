a9116715c99d4ed3ab9599f93a3be6cedfe647d9
Silvia Njoki <10silvianjoki@gmail.com>
2022-02-09 11:21:19 +0300

    add pipe and constructor edit

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any): number {
+    let today:Date = new Date (); 
+    let todayWIthNoTime:any = new Date (today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value - todayWIthNoTime)
+    const secondsInDay = 86400; 
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
 
+    if (dateCounter >=1 && value >todayWIthNoTime){
+      return dateCounter;
+    }else{
+        return 0;
+      } 
+    }
 }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    showInfo:boolean;
+    constructor(public name:string, public quote:string, public author:string, public datePosted:Date, public likes:number, public dislikes:number) {
+        this.showInfo=false
+    }
 }
