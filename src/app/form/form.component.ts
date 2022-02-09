3d42996a1614fb1aced920dcd653cf1ceb8c752f
Silvia Njoki <10silvianjoki@gmail.com>
2022-02-09 11:47:33 +0300

    add images

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { EventEmitter } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+// import { Quotes} from '../quotes'
 
 @Component({
   selector: 'app-form',
@@ -6,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+//   newQuote = new Quotes ('','','',new Date(),0,0);
+// @Output() addQuote = new EventEmitter < Quotes > ();
+
+//   submitQuote(){
+//     this.addQuote.emit(this.newQuote)
+//   }
 
   constructor() { }
 
